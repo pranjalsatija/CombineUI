bf9e71eb289017e20800aa11b46832cb0cbc749e
Pranjal Satija <me@pranj.co>
2020-01-21 22:28:54 -0600

    Initial Commit

---
+++ CombineUI/CombineUI.h
@@ -0,0 +1,19 @@
+//
+//  CombineUI.h
+//  CombineUI
+//
+//  Created by pranjal on 1/21/20.
+//  Copyright © 2020 pranjal. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CombineUI.
+FOUNDATION_EXPORT double CombineUIVersionNumber;
+
+//! Project version string for CombineUI.
+FOUNDATION_EXPORT const unsigned char CombineUIVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CombineUI/PublicHeader.h>
+
+
